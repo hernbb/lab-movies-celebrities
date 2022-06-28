8f92cadb48b77157f481ad9b8b2b016a8fe42ca6
Hernando Bifaretti <hbifaretti@gmail.com>
2022-06-28 19:28:40 +0200

    done-iteration-8

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -6,17 +6,18 @@ const Movie = require("../models/movie.model");
 // all your routes here
 
 router.get("/movies/create", (req, res, next) => {
-   Celebrity.find()
-   .select("name")
-   .select("_id")
-//.select({ name: 1, occupation: 1 })
-  
-   .then((response)=>{
-       console.log(response)
-    res.render("movies/new-movie.hbs", {response});
+  Celebrity.find()
+    .select("name")
+    .select("_id")
+    //.select({ name: 1, occupation: 1 })
 
-  })
-  .catch((e)=>{next(e)})
+    .then((response) => {
+      console.log(response);
+      res.render("movies/new-movie.hbs", { response });
+    })
+    .catch((e) => {
+      next(e);
+    });
 });
 
 router.post("/movies/create", (req, res, next) => {
@@ -33,15 +34,26 @@ router.post("/movies/create", (req, res, next) => {
     .catch(() => res.redirect("movies/new-movie"));
 });
 
-router.get('/movies', (req, res, next) => {
-          
-    Movie.find()
+router.get("/movies", (req, res, next) => {
+  Movie.find()
 
-    .then((response)=>{
-        res.render('movies/movies', {response})
-
+    .then((response) => {
+      res.render("movies/movies", { response });
     })
-    .catch((e)=>{next(e)})
+    .catch((e) => {
+      next(e);
+    });
+});
+
+router.get("/movies/:id", (req, res, next) => {
+  Movie.findById(req.params.id)
+  .populate('cast')
+  .then((response)=>{
+      res.render("movies/movie-details", {response})
   })
+  .catch((e) => {
+    next(e)
+  });
+});
 
 module.exports = router;
